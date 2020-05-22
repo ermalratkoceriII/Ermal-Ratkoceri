89e1f665d0827a83c100208b20f5a7d1df9ed7d5
Ermal Ratkoceri <ermalratkoceri2@gmail.com>
2020-05-22 14:29:03 +0200

    Projekti-Pjesa1

--- Projekti-Pjesa1/Js/Login.js
+++ Projekti-Pjesa1/Js/Login.js
@@ -44,39 +44,31 @@ function validateRegister(){
     var password = document.getElementById('RegPass');
     var email = document.getElementById('email');
 
+    var userpattern = /[A-Z]+[a-z]{6,20}/
+    var passpattern = /[A-Z]+[a-z]{8,20}/
+    var emailpattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}/;
+
     if(username.value.trim() == "" && password.value.trim() == "" &&email.value.trim() == "" ){
         alert('Please fill in your info');
     }else if(email.value.trim() == ""){
         alert('email cant be blank');
         email.focus();
-    }
-    else if(username.value.length < 6){
-        alert('Username needs to be more than 6 characters');
-    }
-    else if(username.value.trim() == ""){
+    }else if(username.value.trim() == ""){
         alert('Username cant be blank');
         username.focus();
-    } else if(password.value.trim() == ""){
+    }else if(!email.value.match(emailpattern)){
+        alert("Please write a valid e-mail, eg example@gmail.com");
+    }
+    else if(!username.value.match(userpattern) || username.value.length > 20){
+        alert('First character uppercase! Username should be longer than 6 characters and shorter than 20 characters. ALERT CANT CONTAIN SYMBOLS');
+    }
+     else if(password.value.trim() == ""){
         alert('Password cant be blank');
         password.focus();
     }
-    else if(password.value.length < 8){
-        alert('Password needs to be more than 8 characters');
+    else if(!password.value.match(passpattern) || password.value.length > 20){
+        alert('First character uppercase! Password needs to be more than 8 characters and less than 20 characters. ALERT CANT CONTAIN SYMBOLS');
     }
-    else{
-        validateemail();
-    }
+   
 }
 
-function validateemail(){  
-var email=document.getElementById('email');  
-var x = email.value;
-
-var pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}/;
-
-if(!x.match(pattern)){
-    alert("Please write a valid e-mail, eg example@gmail.com");
-}
-
-
-} 
